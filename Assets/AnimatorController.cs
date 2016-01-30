de21862e7ecdf829db0055c7403981cdbe721726
Aurelien Gaube <aurelien.gaube8@etu.univ-lorraine.fr>
2016-01-30 11:11:21 +0100

    CANCER

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour {
+
+    public static AnimatorController instance;
+
+    Transform myTrans;
+    Animator myAnim;
+    Vector3 artScaleCache;
+
+	void Start () {
+        myTrans = this.transform;
+        myAnim = this.gameObject.GetComponent<Animator>();
+        instance = this;
+
+        artScaleCache = myTrans.localScale;
+	}
+
+    void FlipArt(float currentSpeed) {
+        if((currentSpeed <0 && artScaleCache.x == 1) || (currentSpeed >0 && artScaleCache.x == -1)) {
+            artScaleCache.x *= -1;
+            myTrans.localScale = artScaleCache;
+        }
+    }
+
+    public void UpdateSpeed(float currentSpeed) {
+        myAnim.SetFloat("Speed", currentSpeed);
+        FlipArt(currentSpeed);
+    }
+
+    public void UpdateIsGround(bool isGrounded) {
+        myAnim.SetBool("isGrounded", isGrounded);
+    }
+
+	void Update () {
+	
+	}
+}
