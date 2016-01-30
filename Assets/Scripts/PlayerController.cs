152a75699aa6f92daafa057b471d1f411d1e44ba
Aurelien Gaube <aurelien.gaube8@etu.univ-lorraine.fr>
2016-01-30 01:51:02 +0100

    Ajout Script Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, -10, 10),
+            0.0f,
+            0.0f
+		);
+	}
+}
